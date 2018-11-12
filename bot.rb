502d8908c26204fab5d94e0512ff7aeec0a24216
Dereck-npo-gurova <dreadmozart@gmail.com>
2018-11-13 00:08:44 +0300

    token

--- bot.rb
+++ bot.rb
@@ -11,9 +11,16 @@ Telegram::Bot::Client.run(token) do |bot|
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     when '/ticket'
-      question = 'Пожалуйста предъявите студенческий токен'
-      answers =
-      bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+      def add(bot, update):
+      bot.send_message(chat_id=update.message.chat_id, text="Пожалуйста предъявите студенческий токен")
+      #for user reply#
+      ticket = update.message.text #get user reply#
+      bot.send_message(chat_id=update.message.chat_id, text=ticket)
+      if ticket = 'UqX8I0ML5jK4VBUrWIDj'
+      bot.send_message(chat_id=update.message.chat.id, text="Спасибо! Все верно!")
+      else
+        bot.send_message(chat_id=update.message.chat.id, text="хуевый токен!")
+      end
     end
   end
 end
