ed8247b1eb5969038bacf9a1780a5ccbc10e22f1
Dereck-npo-gurova <dreadmozart@gmail.com>
2018-11-13 01:42:48 +0300

    fix

--- bot.rb
+++ bot.rb
@@ -3,23 +3,30 @@ require 'coupon_code'
 
 token = '611625592:AAGuFBSmQnFzstpcTXJxo5myx8i1Fpk8FDE'
 
+ANSWERS = [
+  "Правильно"
+]
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Приветствую тебя #{message.from.first_name}! Меня зовут Архивариус Audioskillz. Если вы хотите получить доступ к дополнительным учебным материалам, предъявите студенческий токен, #{message.from.first_name}")
+    when '/start', '/start start'
+        bot.api.send_message(
+          chat_id: message.chat.id,
+          text: "Приветствую тебя #{message.from.first_name}! Меня зовут Архивариус Audioskillz.
+          Если вы хотите получить доступ к дополнительным учебным материалам,
+          воспользуйтесь командой /ticket")
       when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        bot.api.send_message(
+          chat_id: message.chat.id,
+          text: "Пока, #{message.from.first_name}")
       when '/ticket'
-          bot.send_message(chat_id=update.message.chat_id, text="Пожалуйста предъявите студенческий токен")
+        bot.api.send_message(
+          chat_id: update.message.chat.id,
+          text: "Пожалуйста предъявите студенческий токен")
           #for user reply#
           ticket = update.message.text #get user reply#
-          bot.send_message(chat_id=update.message.chat_id, text=ticket)
-            if ticket = 'UqX8I0ML5jK4VBUrWIDj'
-              bot.send_message(chat_id=update.message.chat.id, text="Спасибо! Все верно!")
-              else
-                bot.send_message(chat_id=update.message.chat.id, text="хуевый токен!")
-              end
-            end
+          bot.send_message(
+            chat_id=update.message.chat_id, text=ticket)
         end
       end
