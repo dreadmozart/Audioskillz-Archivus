a468bb8c3555e2a67f214f5a6d3bed6ebc1d0788
Dereck-npo-gurova <dreadmozart@gmail.com>
2018-11-12 22:25:05 +0300

    tickets

--- bot.rb
+++ bot.rb
@@ -6,9 +6,15 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Приветствую вас! Меня зовут Архивариус Audioskillz. Если вы хотите получить доступ к дополнительным учебным материалам, предъявите студенческий токен, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Приветствую тебя #{message.from.first_name}! Меня зовут Архивариус Audioskillz. Если вы хотите получить доступ к дополнительным учебным материалам, предъявите студенческий токен, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/ticket'
+      question = 'Пожалуйста предъявите студенческий токен'
+      answers =
+        Telegram::Bot::Types::ReplyKeyboardMarkup
+        .new(keyboard: [%w(A B), %w(C D)], one_time_keyboard: true)
+      bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
     end
   end
 end
